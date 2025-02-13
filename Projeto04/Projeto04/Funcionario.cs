5001d6ac7f7e79f8b7748bbef50f633a88a6d6e4
guilhermezanollo <guilherme.zanollo@gmail.com>
2025-02-13 16:18:27 -0300

    Nova atualização

---
+++ Projeto04/Projeto04/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double percentual)
+        {
+            SalarioBruto += SalarioBruto * (percentual / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projeto04/Projeto04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Projeto04;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + func);
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double percentual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(percentual);
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
