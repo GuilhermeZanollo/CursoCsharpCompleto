0a3291b887bd75126cbb682ba03db025e8366b1d
guilhermezanollo <guilherme.zanollo@gmail.com>
2025-02-14 16:20:38 -0300

    Atualização

---
+++ Projeto10/Projeto10/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto() //FORMAS DE USAR O CONSTRUTOR
+        {
+        }
+        public Produto(string nome, double preco, int quantidade) // FORMAS DE USAR O CONSTRUTOR
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
