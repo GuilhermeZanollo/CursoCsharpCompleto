c59232617343a464f676fc2e0349354a506e34a6
guilhermezanollo <guilherme.zanollo@gmail.com>
2025-02-17 08:52:11 -0300

    Alteração de nome pasta

---
+++ Projeto11/Encapsulamento/Produto.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome; //quando for trabalhar com atributo privativo, o formato do nome fica com _ e letra minuscula
+        private double _preco; // depois de deixar privativo, é necessário chamar os atributos com método get e set
+        private int _quantidade;
+
+        public Produto() 
+        {
+        }
+        public Produto(string nome, double preco, int quantidade) // FORMAS DE USAR O CONSTRUTOR
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome() // Crio esse método para buscar/consultar nome 
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome) // void pois nao vai adicionar nada como saída, apenas vai alterar o nome
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome; // posso adicionar condicional também
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projeto11/Encapsulamento/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            //p.Quantidade = -10; // SEM O ENCAPSULAMENTO É ISSO QUE O USUÁRIO PODE FAZER, adicionar um valor para quantidade de produtos 
+            // Para isso não ocorrer, devo transformar o atributo QUANTIDADE no tipo PRIVATE
+
+            p.SetNome("TV 4K"); // chamando método set e alterando o nome da TV
+
+            Console.WriteLine(p.GetNome()); // busco o nome com método Get para o atributo nome
+            Console.WriteLine(p.GetPreco()); // Com esses métodos conseguimos controlar como nossa classe deve ser utilizada
+            Console.WriteLine(p.GetQuantidade());
+        }
+    }
+}
